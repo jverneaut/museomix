09b3b0d744718d4fb25d68523a829eea3f6176eb
Julien Verneaut <jverneaut@gmail.com>
2023-11-11 15:59:33 +0100

    Test

--- script.py
+++ script.py
@@ -5,20 +5,27 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-# vlc_instance = vlc.Instance('--no-xlib')
-# vlc_player = vlc_instance.media_player_new()
+vlc_instance = vlc.Instance('--no-xlib')
+vlc_player = vlc_instance.media_player_new()
 
 videos = [
     { 'path': 'sample.mp4', 'pin': 17 }
 ]
 
 for video in videos:
+    video['media'] = vlc_instance.media_new(video['path'])
+
     def sensorCallback(channel):
         print(channel)
         if GPIO.input(video['pin']):
-            print("HIGH")
+            print("Stop video")
         else:
-            print("LOW")
+            print("Play video")
+
+            for video in videos:
+                if video['pin'] == channel:
+                    vlc_player.set_media(video['media'])
+                    vlc_player.play()
 
     GPIO.setup(video['pin'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(video['pin'], GPIO.BOTH, callback=sensorCallback, bouncetime=200)
