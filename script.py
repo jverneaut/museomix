0c8b63fa890ad3b9692901a106f479326f52b85d
Julien Verneaut <jverneaut@gmail.com>
2023-11-11 17:04:30 +0100

    Test

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ videos = [
     {'media_path': 'sample-2.mp4', 'pin': 13},
 ]
 
-instance = vlc.Instance('--no-xlib')
+instance = vlc.Instance('--no-xlib --quiet')
 player = instance.media_list_player_new()
 
 media_list = []
