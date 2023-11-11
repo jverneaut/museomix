f8101928936aaaa916d1e42c7d9458a85e04834d
Julien Verneaut <jverneaut@gmail.com>
2023-11-11 15:08:17 +0100

    Test

--- player.py
+++ player.py
@@ -38,8 +38,8 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-# GPIO.setup(12, GPIO.IN)
+# GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(10, GPIO.IN)
 
 # GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
 
