87974bd60660541e60e25292d39c94bcf56d1c4a
Julien Verneaut <jverneaut@gmail.com>
2023-11-11 13:04:40 +0100

    Add sample code

--- player.py
+++ player.py
@@ -1,7 +1,31 @@
 import vlc
+import time
 
-def play_video(name):
-    player = vlc.MediaPlayer(name)
+def play_video(file_path):
+    # Creating a VLC instance
+    instance = vlc.Instance('--no-xlib')
+
+    # Creating a MediaPlayer with the given instance
+    player = instance.media_player_new()
+
+    # Creating a Media object
+    media = instance.media_new(file_path)
+
+    # Setting the media to the player
+    player.set_media(media)
+
+    # Playing the video
     player.play()
 
-play_video('sample.mp4')
+    # Waiting for the video to finish
+    while player.get_state() != vlc.State.Ended:
+        time.sleep(1)
+
+    # Stopping the player
+    player.stop()
+
+if __name__ == "__main__":
+    # Replace 'path/to/your/video/file.mp4' with the actual path to your video file
+    video_path = 'sample.mp4'
+
+    play_video(video_path)
