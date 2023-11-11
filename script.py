73051555713b1879643d56c6bd21517b57937d42
Julien Verneaut <jverneaut@gmail.com>
2023-11-11 16:32:14 +0100

    Test

--- script.py
+++ script.py
@@ -5,26 +5,31 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-vlc_instance = vlc.Instance('--no-xlib')
-vlc_player = vlc_instance.media_player_new()
-
 videos = [
-    { 'media': vlc_instance.media_new('sample.mp4'), 'pin': 11 },
-    { 'media': vlc_instance.media_new('sample-2.mp4'), 'pin': 13 },
+    {'media': vlc.MediaPlayer('sample.mp4'), 'pin': 11},
+    {'media': vlc.MediaPlayer('sample-2.mp4'), 'pin': 13},
 ]
 
 for video in videos:
+    video['player'] = vlc.MediaPlayer(video['media'].get_mrl())
+
     def sensorCallback(channel):
         if GPIO.input(video['pin']):
             for item in videos:
                 if item['pin'] == channel:
-                    vlc_player.set_media(item['media'])
-                    vlc_player.play()
+                    item['player'].play()
         else:
-            vlc_player.pause()
+            for item in videos:
+                item['player'].pause()
 
     GPIO.setup(video['pin'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(video['pin'], GPIO.BOTH, callback=sensorCallback, bouncetime=200)
 
-while True:
-    pass
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    # Cleanup on program exit
+    for video in videos:
+        video['player'].stop()
+    GPIO.cleanup()
