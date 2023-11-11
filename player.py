68f68c6584ace2b83bc819c3ca5ed75a75ff9103
Julien Verneaut <jverneaut@gmail.com>
2023-11-11 13:12:01 +0100

    Test GPIO

--- player.py
+++ player.py
@@ -1,31 +1,31 @@
 import vlc
 import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 def play_video(file_path):
-    # Creating a VLC instance
     instance = vlc.Instance('--no-xlib')
-
-    # Creating a MediaPlayer with the given instance
     player = instance.media_player_new()
 
-    # Creating a Media object
     media = instance.media_new(file_path)
-
-    # Setting the media to the player
     player.set_media(media)
 
-    # Playing the video
     player.play()
 
-    # Waiting for the video to finish
     while player.get_state() != vlc.State.Ended:
         time.sleep(1)
 
-    # Stopping the player
     player.stop()
 
-if __name__ == "__main__":
-    # Replace 'path/to/your/video/file.mp4' with the actual path to your video file
-    video_path = 'sample-2.mp4'
+# if __name__ == "__main__":
+#     video_path = 'sample-2.mp4'
 
-    play_video(video_path)
+#     play_video(video_path)
+
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
