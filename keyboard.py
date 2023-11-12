4e2b3f28ea0cb95c39b99e19a23dff96ebc73f75
Julien Verneaut <jverneaut@gmail.com>
2023-11-12 15:21:18 +0100

    Test

--- keyboard.py
+++ keyboard.py
@@ -1,6 +1,6 @@
-# from pynput import keyboard
-import keyboard
 import vlc
+import pygame
+from pygame.locals import KEYDOWN, K_LEFT, K_DOWN
 
 instance = vlc.Instance('--no-xlib --quiet')
 player = instance.media_list_player_new()
@@ -14,25 +14,23 @@ media_list.append('video-4.mp4')
 
 player.set_media_list(instance.media_list_new(media_list))
 
-# def on_press(key):
-#     if key == keyboard.Key.up:
-#         player.play_item_at_index(0)
-#     if key == keyboard.Key.down:
-#         player.play_item_at_index(1)
-#     if key == keyboard.Key.left:
-#         player.play_item_at_index(2)
-#     if key == keyboard.Key.left:
-#         player.play_item_at_index(3)
 
-# with keyboard.Listener(on_press=on_press) as listener:
-#     listener.join()
+pygame.init()
 
-while True:
-    if keyboard.read_key() == "w":
-        print("You pressed w")
-    if keyboard.read_key() == "a":
-        print("You pressed a")
-    if keyboard.read_key() == "s":
-        print("You pressed s")
-    if keyboard.read_key() == "d":
-        print("You pressed d")
+# Set up the display (you can skip this if you don't need a window)
+screen = pygame.display.set_mode((200, 200))
+pygame.display.set_caption("Arrow Key Detection")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                print("Left arrow key pressed")
+            if event.key == K_DOWN:
+                print("Down arrow key pressed")
+
+pygame.quit()
